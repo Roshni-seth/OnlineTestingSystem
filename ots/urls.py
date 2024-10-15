287f10fa5cd3bd9922a509630eab75362c77b575
Roshni-seth <168473765+Roshni-seth@users.noreply.github.com>
2024-10-15 12:23:22 +0530

    Initial commit

---
+++ ots/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from ots.views import *
+app_name='ots'
+urlpatterns = [
+    path('', welcome),
+    path('new-candidate',candidateRegistrationForm,name='registrationForm'),
+    path('store-candidate',candidateRegistration,name='storeCandidate'),
+    path('login',loginView,name='login'),
+    path('home',candidateHome, name='home'),
+    path('test-paper',testPaper,name='testPaper'),
+    path('calculate-result',calculateTestResult,name='calculateTest'),
+    path('test-history',testResultHistory,name='testHistory'),
+    path('result',showTestResult,name='result'),
+    path('logout',logoutView,name='logout'),
+]
